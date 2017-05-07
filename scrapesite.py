2677d74610341b9a2ffa4676ff9e7c7c7384a946
EC2 Default User <ec2-user@ip-172-31-17-129.us-west-2.compute.internal>
2017-05-07 19:20:35 +0000

    Added comments, small syntax changes

--- scrapesite.py
+++ scrapesite.py
@@ -6,6 +6,7 @@ import pickle
 import time
 import smtplib
 
+#argument parsing
 parser = argparse.ArgumentParser()
 parser.add_argument("substr", help="substr to look for")
 parser.add_argument("siteurl", help="url of site to scrape")
@@ -15,21 +16,24 @@ args = parser.parse_args()
 if args.t:
     print('testing')
 
+
 url = args.siteurl
 r = requests.get("https://"+url)
 data = r.text
 soup = BeautifulSoup(data, "html.parser")
 msg = ''
 
+#if file doesnt exist, initialize with empty file
 try:
     with open('senturls' + args.substr + '.pickle', 'rb') as handle:
         senturls = pickle.load(handle)
 except FileNotFoundError:
     if args.t:
-        print('exception was hit')
+        print('FileNotFoundError caught')
     senturls = set()
 
-
+#look through all h5 tags, determine validity
+#this is the part to modify if you want to make this work for not twitch
 for i in soup.find_all("h5"):
     if args.substr.lower() in i.string.lower():
         jobname = i.string
@@ -42,9 +46,12 @@ for i in soup.find_all("h5"):
             senturls.add(joblink)
             msg += jobname + '\n' + joblink + '\n'
 
+#dump back into pickle file
 with open('senturls' + args.substr + '.pickle', 'wb') as handle:
     pickle.dump(senturls, handle, pickle.HIGHEST_PROTOCOL)
 
+#smtp request
+#if testing, will not actually send but will try to do all other steps
 if msg != '':
     msg += 'Completed ' + time.strftime("%H:%M:%S") + '\n'
     if args.t:
