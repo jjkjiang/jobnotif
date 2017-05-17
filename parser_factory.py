bb0f5354f8c92ab5e9efdbbfac991d61ee0c3599
Jerry Jiang <jerryjiekaijiang@gmail.com>
2017-05-16 22:05:02 -0700

    Update parser_factory.py

--- parser_factory.py
+++ parser_factory.py
@@ -3,6 +3,7 @@ from parsers.twitch_parser import TwitchParser
 class ParserFactory:
     def build(self, type, bsoup, senturls, substr, t):
         temp = type.lower().replace(" ", "")
+        temp = temp.replace("_", "")
         if temp == "twitchparser":
             return TwitchParser(bsoup, senturls, substr, t)
         else:
